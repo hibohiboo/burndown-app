8f899eedc7e32b8a3cb3254be4b5d1090e96ccc3
hibohiboo <swdice66@gmail.com>
2020-01-25 01:30:55 +0900

    add firestoreへの通信： バグ：無限更新

--- src/index.js
+++ src/index.js
@@ -5,8 +5,7 @@ import { library } from '@fortawesome/fontawesome-svg-core';
 import { fab } from '@fortawesome/free-brands-svg-icons';
 import { fas } from '@fortawesome/free-solid-svg-icons';
 import { far } from '@fortawesome/free-regular-svg-icons';
-import firebase from 'firebase/app';
-import loginUi from './firebase/ui';
+import { loginUi, auth } from './firebase';
 import * as serviceWorker from './serviceWorker';
 import Store from './store/index';
 import App from './component/App';
@@ -22,11 +21,6 @@ function init() {
   );
 }
 
-// firebase使用準備
-firebase.initializeApp(require('./firebase/config'));
-
-const auth = firebase.auth();
-
 // 認証済かチェック
 auth.onAuthStateChanged((payload) => {
   if (!payload) {
@@ -38,9 +32,6 @@ auth.onAuthStateChanged((payload) => {
   // 認証していたらレンダリング
   init();
 });
-
-// サインアウト時の動作確認
-// auth.signOut();
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
