08a7fdb936aa1d7a781acb55c171f7e2a7b22bf2
hibohiboo <swdice66@gmail.com>
2020-01-30 01:16:45 +0900

    fix immerの導入

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,3 +1,4 @@
+import produce from 'immer';
 import {
   EDIT_TASK, DELETE_TASK, ADD_TASK, EDIT_VELOCITY, ADD_SPRINT,
   DELETE_SPRINT,
@@ -12,74 +13,62 @@ const setTaskIDHelper = (array) => Object.assign([], array.map((data, index) => 
   return { ...data, id: `data${id}` };
 }));
 
-function rootReducer(state = init, action) {
+const rootReducer = produce((draft = init, action) => {
   // タスク編集時
   if (action.type === EDIT_TASK) {
-    return {
-      ...state,
-      datas: Object.assign([], state.datas.map((data) => {
-        if (data.id === action.payload.id) {
-          return { ...data, ...action.payload };
-        }
-        return data;
-      })),
-    };
+    draft.datas = draft.datas.map((data) => {
+      if (data.id === action.payload.id) {
+        return { ...data, ...action.payload };
+      }
+      return data;
+    });
+    return draft;
   }
 
   // タスク削除時
   if (action.type === DELETE_TASK) {
-    return {
-      ...state,
-      datas: setTaskIDHelper(state.datas.filter((data) => (data.id !== action.payload.id))),
-    };
+    draft.datas = setTaskIDHelper(draft.datas.filter((data) => (data.id !== action.payload.id)));
+    return draft;
   }
 
   // タスク追加時
   if (action.type === ADD_TASK) {
-    return {
-      ...state,
-      datas: setTaskIDHelper(state.datas.concat(initialData)),
-    };
+    draft.datas = setTaskIDHelper(draft.datas.concat(initialData));
+    return draft;
   }
 
   // velocity編集時
   if (action.type === EDIT_VELOCITY) {
-    return {
-      ...state,
-      sprints: Object.assign([], state.sprints.map((sprint) => {
-        if (sprint.id === action.payload.id) {
-          return { ...sprint, ...action.payload };
-        }
-        return sprint;
-      })),
-    };
+    draft.sprints = draft.sprints.map((sprint) => {
+      if (sprint.id === action.payload.id) {
+        return { ...sprint, ...action.payload };
+      }
+      return sprint;
+    });
+    return draft;
   }
 
   // sprint追加時
   if (action.type === ADD_SPRINT) {
-    return {
-      ...state,
-      sprints: Object.assign([], state.sprints.concat({
-        id: `id${state.sprints.length + 1}`,
-        start: '',
-        end: '',
-        planningCapacity: '0',
-        resultCapacity: '0',
-        velocity: '',
-      })),
-    };
+    draft.sprints = draft.sprints.concat({
+      id: `id${draft.sprints.length + 1}`,
+      start: '',
+      end: '',
+      planningCapacity: '0',
+      resultCapacity: '0',
+      velocity: '',
+    });
+    return draft;
   }
 
   // sprint削除時
   if (action.type === DELETE_SPRINT) {
-    return {
-      ...state,
-      sprints: Object.assign([], state.sprints.slice(0, state.sprints.length - 1)),
-    };
+    draft.sprints = draft.sprints.slice(0, draft.sprints.length - 1);
+    return draft;
   }
 
   // 初期表示
-  return state;
-}
+  return draft;
+});
 
 export default rootReducer;
