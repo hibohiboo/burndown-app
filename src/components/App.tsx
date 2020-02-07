2ea44ccac2a0f8c0e0641d4ed101d56c744ec8aa
hibohiboo <swdice66@gmail.com>
2020-02-07 21:21:35 +0900

    add app libraries

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,9 +1,34 @@
-import React from "react";
+import React from 'react';
+import {
+  BrowserRouter as Router, Switch, Route, Link, Redirect,
+} from 'react-router-dom';
+import Helmet from 'react-helmet';
 
 const App: React.FC = () => {
-  return <div>
-    test
-  </div>;
+  return   <div className="App">
+  <Helmet>
+    <meta charSet="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0" />
+    <title>BurnDown App</title>
+  </Helmet>
+  <Router>
+    <header>
+      <Link to="/burndown">BurnDown</Link>
+      |
+      <Link to="/velocity">Velocity</Link>
+      |
+      <Link to="/taskboard">TaskBoard</Link>
+    </header>
+    <Switch>
+      <Route exact path="/">
+        <Redirect to="/burndown" />
+      </Route>
+      <Route path="/*">
+        <p>404</p>
+      </Route>
+    </Switch>
+  </Router>
+</div>;
 };
 
 export default App;
